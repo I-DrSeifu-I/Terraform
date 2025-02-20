4725f19ed47848066b2d3b0ee3719691572b72e2
I-DrSeifu-I <124513468+I-DrSeifu-I@users.noreply.github.com>
2025-02-20 13:55:02 -0500

    Update data.tf

--- Azure/AVD Deployment/data.tf
+++ Azure/AVD Deployment/data.tf
@@ -1,1 +1,11 @@
+#key vault used for local admin creds
+data "azurerm_key_vault" "KVforLocaladmin" {
+  name                = var.keyvaultname
+  resource_group_name = var.keyvaultRG
+}
 
+#key vault secret for local admin password
+data "azurerm_key_vault_secret" "KVsecret" {
+  name         = var.keyvaultsecretname
+  key_vault_id = data.azurerm_key_vault.KVforLocaladmin.id
+}
