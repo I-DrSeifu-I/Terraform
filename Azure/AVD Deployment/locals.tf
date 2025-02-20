b82c9f378ee049d3660c1940d1010fba4d8f833d
I-DrSeifu-I <124513468+I-DrSeifu-I@users.noreply.github.com>
2025-02-20 13:21:48 -0500

    Update locals.tf

--- Azure/AVD Deployment/locals.tf
+++ Azure/AVD Deployment/locals.tf
@@ -1,1 +1,5 @@
-
+locals {
+  current_time_for_tag = formatdate("MM-DD-YY", timestamp())
+  current_time = timestamp()
+  expiration_duration = "720h" # Example for 30 days. Adjust the duration as needed.
+}
